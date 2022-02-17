079f48f46ee6111030a583a3f725655dc7eb7fc7
cristiam <cristiam9211@gmail.com>
2022-02-17 17:26:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/movierecomendation/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,29 @@
+package com.example.movierecomendation.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.movierecomendation.R
+import com.example.movierecomendation.databinding.FragmentDashboardBinding
+
+class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
+
+    private var _binding: FragmentDashboardBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/movierecomendation/ui/home/HomeFragment.kt
@@ -0,0 +1,28 @@
+package com.example.movierecomendation.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.movierecomendation.R
+import com.example.movierecomendation.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
